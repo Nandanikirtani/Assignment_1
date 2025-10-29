5da4a27cf859816368ee6103b899a9eb7013cd13
Nandani Kirtani <160708805+Nandanikirtani@users.noreply.github.com>
2025-10-29 14:50:33 +0530

    Setup of cors according to frontend deployed link

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,24 @@ dotenv.config();
 connectDB();
 
 const app = express();
-app.use(cors());
+const allowedOrigins = [
+  "http://localhost:5173", // local frontend (Vite default)
+  "https://assignment-1-ten-plum.vercel.app", // deployed frontend
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      // Allow requests with no origin (like Postman)
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 // Routes
